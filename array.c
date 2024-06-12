f76ae11e4e033a987b2d7c276d47068aae4bb333
roushan381 <41905843+roushan381@users.noreply.github.com>
2024-06-12 14:35:45 +0000

    union

--- array.c
+++ array.c
@@ -6,6 +6,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
+#if 0
 #include <stdio.h>
 
 int main()
@@ -15,3 +16,36 @@ int main()
 
     return 0;
 }
+#else
+#include <stdio.h>
+
+int min(int a[], int n)
+{
+    for(int i=0; i<n;i++)
+    {
+        printf("min 1 value %d : %d\n",i,a[i]);
+    }
+    printf("\n");
+    a[0]=a[0]+a[1];
+    a[1]=a[0]-a[1];
+    a[0]=a[0]-a[1];
+    a[4]=20;
+    
+    for(int i=0; i<n;i++)
+    {
+        printf("min 2 value %d : %d\n",i,a[i]);
+    }
+}
+
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    printf("Hello World %d\n",min(a,5));
+    for(int i=0; i<5;i++)
+    {
+        printf("main 1value %d : %d\n",i,a[i]);
+    }
+
+    return 0;
+}
+#endif
