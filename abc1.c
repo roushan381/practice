db82f8ffa0afc7636572e15e713562d44166183e
roushan381 <41905843+roushan381@users.noreply.github.com>
2024-06-12 14:35:45 +0000

    union

--- abc1.c
+++ abc1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int a=10;
 
-typedef struct Student
+typedef struct
 {
     char name[30];
-    int age;
+    const int age;
     /* here Address is a structure */
-    struct Address
+    struct
     {
         char locality[50];
         char city[50];
@@ -14,15 +14,26 @@ typedef struct Student
     }addr;
 }Student_t;
 
+typedef struct 
+{
+    int a;
+}struct1;
+
 int main()
 {
     a=20;
+    struct1 s2;
+    // Student_t s1;
     Student_t s1={"a1",20,"home","blr",560000};
+    // s1.age=10;
     printf("name %s\n",s1.name);
     printf("age %d\n",s1.age);
+    // s1.age=30;
+    // printf("age %d\n",s1.age);
     printf("loc %s\n",s1.addr.locality);
     printf("city %s\n",s1.addr.city);
     printf("pin %d\n",s1.addr.pincode);
+    printf("size %d\n",sizeof(s1.name));
 
     // ab();
     // printf("main %d\n",a);
