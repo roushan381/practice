2ebf74ef2f08b948d85b1f6973be452daac62bfb
roushan381 <41905843+roushan381@users.noreply.github.com>
2024-07-03 11:51:38 +0000

    bubble modified

--- bubble.c
+++ bubble.c
@@ -2,15 +2,25 @@
 #include<stdlib.h>
 
 void bubble(int a[], int n);
+void swap(int *a, int *b);
+int sorted_array=1;
 int main()
 {
-    int a[5]={5,4,3,2,1};
+    #if 0
+    int a[5]={5,4,3,2,55};
+    #else
+    int a[5]={11,2,4,3,5};
+    #endif
     int n=sizeof(a)/sizeof(a[0]);
     for(int i=0;i<n;i++)
     {
         printf("before value %d\n",a[i]);
     }
     bubble(a, n);
+    if(sorted_array)
+        printf("already sorted\n");
+    else
+        printf("unsorted\n");
     for(int i=0;i<n;i++)
     {
         printf("after value %d\n",a[i]);
@@ -24,10 +34,15 @@ void bubble(int a[], int n)
     {
         for(int i=0;i<n-1-j;i++){
             if(a[i]>a[i+1]){
-                a[i]=a[i]+a[i+1];
-                a[i+1]=a[i]-a[i+1];
-                a[i]=a[i]-a[i+1];
+                swap(&a[i],&a[i+1]);
             }
         }
     }
+}
+
+void swap(int *a, int *b){
+        *a=*a+*b;
+        *b=*a-*b;
+        *a=*a-*b;
+        sorted_array =0;
 }
